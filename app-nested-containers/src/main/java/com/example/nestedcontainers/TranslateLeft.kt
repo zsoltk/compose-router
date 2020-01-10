9411e0aa91bd5098a039cd4613ff16ddf7072a39
Andrei Shikov <andrei.shikov@magiclab.co>
2020-01-10 01:59:52 +0000

    Setup opacity and hardcode enter/exit

--- app-nested-containers/src/main/java/com/example/nestedcontainers/TranslateLeft.kt
+++ app-nested-containers/src/main/java/com/example/nestedcontainers/TranslateLeft.kt
@@ -1,20 +1,21 @@
 package com.example.nestedcontainers
 
-import androidx.animation.AnimationEndReason.TargetReached
 import androidx.animation.FloatPropKey
+import androidx.animation.Spring.Companion.DampingRatioHighBouncy
 import androidx.animation.TransitionDefinition
 import androidx.animation.TransitionState
 import androidx.animation.transitionDefinition
 import androidx.compose.Composable
 import androidx.compose.Key
-import androidx.compose.effectOf
 import androidx.compose.memo
-import androidx.compose.onCommit
 import androidx.compose.unaryPlus
-import androidx.ui.animation.animatedFloat
 import androidx.ui.core.Alignment.TopLeft
 import androidx.ui.core.Draw
+import androidx.ui.core.Opacity
 import androidx.ui.layout.Stack
+import com.example.nestedcontainers.AnimationParams.Opacity
+import com.example.nestedcontainers.AnimationParams.X
+import com.example.nestedcontainers.AnimationParams.Y
 import com.example.nestedcontainers.TransitionStates.Finish
 import com.example.nestedcontainers.TransitionStates.Start
 
@@ -23,8 +24,8 @@ fun <T : Any> Tranlate(current: T, children: @Composable() (T) -> Unit) {
     val animState = +memo { AnimationState<T>() }
     val transitionDefinition = +memo {
         TransitionDef(
-            enterTransition = transitionDef(TranslateParams(startX = 1f, endX = 0f)),
-            exitTransition = transitionDef(TranslateParams(startY = 0f, endY = -1f))
+            enterTransition = enterAnim,
+            exitTransition = exitAnim
         )
     }
     if (animState.current != current) {
@@ -39,12 +40,12 @@ fun <T : Any> Tranlate(current: T, children: @Composable() (T) -> Unit) {
                     toState = Finish,
                     onStateChangeFinished = {
                         if (it == Finish && animState.current == current) {
-//                        println("Cleanup for ${current.javaClass.simpleName}")
+                            //println("Cleanup for ${current.javaClass.simpleName}")
                             animState.items.removeAll { it.key != animState.current }
                         }
                     }
                 ) {
-                    //                    println("Render exit children for ${key.javaClass.simpleName} ${animState.items.size} ${it[X]}")
+                    //println("Render exit children for ${key.javaClass.simpleName} ${animState.items.size} ${it[X]}")
                     children(it)
                 }
             }
@@ -56,7 +57,7 @@ fun <T : Any> Tranlate(current: T, children: @Composable() (T) -> Unit) {
                 initState = if (animState.items.size == 1) Finish else Start,
                 toState = Finish
             ) {
-                //                println("Render enter children for ${current.javaClass.simpleName} ${animState.items.size} ${it[X]}")
+                //println("Render enter children for ${current.javaClass.simpleName} ${animState.items.size} ${it[X]}")
                 children(it)
             }
         }
@@ -66,21 +67,24 @@ fun <T : Any> Tranlate(current: T, children: @Composable() (T) -> Unit) {
         aligned(TopLeft) {
             animState.items.forEach { item ->
                 Key(item.key) {
-                    item.transition {
+                    item.transition { transitionState ->
                         val composable = @Composable() {
                             children(item.key)
                         }
-                        val xFraction = it[X]
-                        val yFraction = it[Y]
-                        Draw(children = composable) { canvas, parentSize ->
-                            canvas.save()
-                            val startX = parentSize.width.value
-                            val startY = parentSize.width.value
+                        val xFraction = transitionState[X]
+                        val yFraction = transitionState[Y]
+                        val opacityFraction = transitionState[Opacity]
+                        Opacity(opacity = opacityFraction) {
+                            Draw(children = composable) { canvas, parentSize ->
+                                canvas.save()
+                                val startX = parentSize.width.value
+                                val startY = parentSize.width.value
 //            println("$startX $endX $floatValue ${(endX - startX) * floatValue}")
-                            canvas.nativeCanvas.clipRect(0f, 0f, parentSize.width.value, parentSize.height.value)
-                            canvas.translate(startX * xFraction, startY * yFraction)
-                            drawChildren()
-                            canvas.restore()
+                                canvas.nativeCanvas.clipRect(0f, 0f, parentSize.width.value, parentSize.height.value)
+                                canvas.translate(startX * xFraction, startY * yFraction)
+                                drawChildren()
+                                canvas.restore()
+                            }
                         }
                     }
                 }
@@ -89,13 +93,6 @@ fun <T : Any> Tranlate(current: T, children: @Composable() (T) -> Unit) {
 
     }
 }
-
-class TranslateParams(
-    val startX: Float = 0f,
-    val endX: Float = 0f,
-    val startY: Float = 0f,
-    val endY: Float = 0f
-)
 
 private class AnimationState<T> {
     var current: T? = null
@@ -107,46 +104,53 @@ private class AnimationItem<T>(
     val transition: @Composable() (@Composable() (TransitionState) -> Unit) -> Unit
 )
 
-private val X = FloatPropKey()
-private val Y = FloatPropKey()
+object AnimationParams {
+    val X = FloatPropKey()
+    val Y = FloatPropKey()
+    val Opacity = FloatPropKey()
+}
 
 private enum class TransitionStates {
     Start, Finish
 }
 
-private fun transitionDef(params: TranslateParams) = transitionDefinition {
+private val enterAnim = transitionDefinition {
     state(Start) {
-        this[X] = params.startX
-        this[Y] = params.startY
+        this[X] = 1f
+        this[Y] = 0f
+        this[Opacity] = 0f
     }
 
     state(Finish) {
-        this[X] = params.endX
-        this[Y] = params.endY
+        this[X] = 0f
+        this[Y] = 0f
+        this[Opacity] = 1f
     }
 
     transition {
-        X using tween { duration = 300 }
+        X using physics { dampingRatio = DampingRatioHighBouncy }
+        X using tween { duration = 1000 }
+        Opacity using tween { duration = 1000 }
     }
 }
 
-private fun animated(init: Float, target: Float, snap: Boolean, onFinish: () -> Unit) = effectOf<Float> {
-    if (snap) return@effectOf target
-
-    val float = +animatedFloat(init)
-
-    +onCommit {
-        float.animateTo(target) { reason, _ ->
-            if (reason == TargetReached) {
-                onFinish()
-            }
-        }
-//        onDispose {
-//            float.stop()
-//        }
+private val exitAnim = transitionDefinition {
+    state(Start) {
+        this[X] = 0f
+        this[Y] = 0f
+        this[Opacity] = 1f
     }
 
-    float.value
+    state(Finish) {
+        this[X] = 0f
+        this[Y] = 1f
+        this[Opacity] = 0f
+    }
+
+    transition {
+        Y using tween { duration = 1000 }
+        Opacity using tween { duration = 1000 }
+    }
 }
 
 private class TransitionDef(
