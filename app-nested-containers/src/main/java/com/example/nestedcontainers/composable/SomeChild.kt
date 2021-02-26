3a2d7caaab2909e2ab8ea653da2d465826b46831
arturm <arturm@hexagontech.co.il>
2021-02-26 13:08:09 +0200

    app-nested-containers sample migrated

--- app-nested-containers/src/main/java/com/example/nestedcontainers/composable/SomeChild.kt
+++ app-nested-containers/src/main/java/com/example/nestedcontainers/composable/SomeChild.kt
@@ -1,6 +1,5 @@
 package com.example.nestedcontainers.composable
 
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +9,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
